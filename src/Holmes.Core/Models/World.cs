c34a747d3d1c691983b7f604f8b94cdb088a9322
Stephen Donaghy <stephen.donaghy@iccsolutions.com>
2016-07-13 20:35:12 +0100

    Added world class to contain the world of our investigation

---
+++ src/Holmes.Core/Models/World.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="World.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Our World class contains everything that's in our world, such as rooms, investigators and suspects
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Holmes.Core.Models
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Our World class contains everything that's in our world, such as rooms, investigators and suspects
+    /// </summary>
+    public class World
+    {
+        /// <summary>
+        /// Gets or sets the list of interrogation rooms within the world
+        /// </summary>
+        public List<InterrogationRoom> InterrogationRooms { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of investigators present within the world
+        /// </summary>
+        public List<Investigator> Investigators { get; set; }
+    }
+}
