884ce5ebe64ca228c8b9482319908d252b847e23
Stephen Donaghy <stephen.donaghy@iccsolutions.com>
2016-07-13 20:48:25 +0100

    First definition of the Investigation service interface

---
+++ src/Holmes.Core/Interfaces/IInvestigationService.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IInvestigationService.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The InvestigationService interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Holmes.Core.Interfaces
+{
+    using System;
+
+    /// <summary>
+    /// The InvestigationService interface.
+    /// </summary>
+    public interface IInvestigationService
+    {
+        /// <summary>
+        /// Moves the "world" along by the defined Timespan
+        /// </summary>
+        /// <param name="timeToInvestigate">How far to move the world along by</param>
+        void Investigate(TimeSpan timeToInvestigate);
+    }
+}
