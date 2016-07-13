bcbb8d9bfb1c770ee06bc5a512a72c0350f6973e
Stephen Donaghy <stephen.donaghy@iccsolutions.com>
2016-07-13 20:07:28 +0100

    Added base solution layout

---
+++ src/Holmes.Core/BaseClasses/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holmes.Core.BaseClasses
+{
+    /// <summary>
+    /// Suspects and investigators will have a base class,
+    /// Actor is that base class
+    /// </summary>
+    public class Actor
+    {
+        /// <summary>
+        /// Gets or sets The name of the actor
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
