ed17bf0fb5aac5ebfae4b65d1b78a8a2d1186910
Stephen Donaghy <stephen.donaghy@iccsolutions.com>
2016-07-13 20:23:31 +0100

    Added interrogation rooms

---
+++ src/Holmes.Core/Models/InterrogationRoom.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InterrogationRoom.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The interrogation room, which contains a single suspect and potentially
+//   an investigator
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Holmes.Core.Models
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The interrogation room, which contains a single suspect and potentially
+    /// an investigator
+    /// </summary>
+    public class InterrogationRoom
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InterrogationRoom"/> class. 
+        /// </summary>
+        public InterrogationRoom()
+        {
+            this.Investigators = new List<Investigator>();
+        }
+
+        /// <summary>
+        /// Gets or sets the current suspect present in the room
+        /// </summary>
+        public Suspect Suspect { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of Investigators in the room
+        /// </summary>
+        public List<Investigator> Investigators { get; set; } // = new List<Investigator>(); // C# 6 feature :(
+    }
+}
