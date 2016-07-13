e59d8be34ee522b70368f2b7c7ccbff196bfd5e3
Stephen Donaghy <stephen.donaghy@iccsolutions.com>
2016-07-13 20:09:52 +0100

    Added suspect class

---
+++ src/Holmes.Core/Models/Suspect.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Suspect.cs" company="None">
+//   Use as you see fit
+// </copyright>
+// <summary>
+//   The suspect of the investigation
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Holmes.Core.Models
+{
+    using Holmes.Core.BaseClasses;
+
+    /// <summary>
+    /// The suspect of the investigation
+    /// </summary>
+    public class Suspect : Actor
+    {
+    }
+}
