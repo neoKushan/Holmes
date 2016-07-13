0f6a41c6a1aff55bf7a519cf2331a545daf77a8c
Stephen Donaghy <stephen.donaghy@iccsolutions.com>
2016-07-13 22:09:16 +0100

    Defined the interrogation service interface

--- src/Holmes.Core/Interfaces/IInvestigationService.cs
+++ src/Holmes.Core/Interfaces/IInvestigationService.cs
@@ -10,6 +10,25 @@
 namespace Holmes.Core.Interfaces
 {
     using System;
+    using System.Text;
+
+    using Holmes.Core.Models;
+
+    /// <summary>
+    /// The investigation result.
+    /// </summary>
+    public enum InvestigationResult
+    {
+        /// <summary>
+        /// The investigation is currently inconclusive.
+        /// </summary>
+        Inconclusive,
+
+        /// <summary>
+        /// The suspect has been found.
+        /// </summary>
+        SuspectFound
+    }
 
     /// <summary>
     /// The InvestigationService interface.
@@ -19,7 +38,18 @@ namespace Holmes.Core.Interfaces
         /// <summary>
         /// Moves the "world" along by the defined Timespan
         /// </summary>
-        /// <param name="timeToInvestigate">How far to move the world along by</param>
-        void Investigate(TimeSpan timeToInvestigate);
+        /// <param name="world">
+        /// The world to investigate.
+        /// </param>
+        /// <param name="timeToInvestigate">
+        /// How far to move the world along by
+        /// </param>
+        /// <param name="investigationText">
+        /// The text results of the investigation step
+        /// </param>
+        /// <returns>
+        /// The <see cref="InvestigationResult"/> of the investigation step.
+        /// </returns>
+        InvestigationResult Investigate(World world, TimeSpan timeToInvestigate, ref StringBuilder investigationText);
     }
 }
